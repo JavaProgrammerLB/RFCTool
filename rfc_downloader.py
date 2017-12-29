c1a8cbf10e148d0c4444d35258a4336b5d8b697a
刘备 <admin@yitianyigexiangfa.com>
2017-12-29 12:40:25 +0800

    update file name

--- rfc_downloader.py
+++ rfc_downloader.py
@@ -1,13 +1,24 @@
 #! /usr/local/bin/python3
 import requests
 import sys
+from bs4 import BeautifulSoup
 
 argvs = sys.argv
+usage_string = "[ERROR] Usage: python rfc_downloader.py [rfc number]"
 if len(argvs) < 2: 
-    print("Usage: ./rfc_downloader.py [rfc number]")
+    print(usage_string)
 else:
     rfc_num = argvs[1]
-    res = requests.get("https://tools.ietf.org/pdf/rfc{}.pdf".format(rfc_num))
-    file = open("rfc{}.pdf".format(rfc_num), "wb")
-    file.write(res.content)
-    print("write finished")
+    if not rfc_num.isdigit():
+        print(usage_string, "rfc number={}, is not a digit".format(rfc_num))
+    else:
+        res = requests.get("https://tools.ietf.org/pdf/rfc{}".format(rfc_num))
+        # get doc name
+        res_html = requests.get("https://tools.ietf.org/html/rfc{}".format(rfc_num))
+        soup = BeautifulSoup(res_html.text, "lxml")
+        doc_name = soup.title.string
+        # write file
+        file_name = "{}.pdf".format(doc_name)
+        file = open(file_name, "wb")
+        file.write(res.content)
+        print("Downloaded {}".format(file_name))
